afbef54a2a160909bf7882163d79cd0d2282d0fc
chotkos <chotkos@gmail.com>
2020-02-11 10:32:24 +0100

    init

---
+++ precache-manifest.ca33128f72619f5edf4f4d7ef5968f4a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68f9ea9b07a74d63fe9d2c686e12f60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cb6a80822e9ab3e44ad",
+    "url": "/static/js/2.47231dfa.chunk.js"
+  },
+  {
+    "revision": "9e2618e2d59f540c83fb",
+    "url": "/static/js/main.65cdb187.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
